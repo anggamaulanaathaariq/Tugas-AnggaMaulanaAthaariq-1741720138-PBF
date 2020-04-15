5ef21c985986bbe60f5748cdbe0f750fddc7ad5d
Angga Maulana Athaariq <am.athaariq@gmail.com>
2020-04-15 12:51:25 +0700

    Modul 10 React FireBase

---
+++ pertemuan-kesebelas/react-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBg_bvEmS7VZ9IkU7Nu3EtU89e71QekAL0",
+  authDomain: "pbf-firebase-anggamaulana.firebaseapp.com",
+  databaseURL: "https://pbf-firebase-anggamaulana.firebaseio.com",
+  projectId: "pbf-firebase-anggamaulana",
+  storageBucket: "pbf-firebase-anggamaulana.appspot.com",
+  messagingSenderId: "327973527006",
+  appId: "1:327973527006:web:e62575ad86780941c8914f",
+};
