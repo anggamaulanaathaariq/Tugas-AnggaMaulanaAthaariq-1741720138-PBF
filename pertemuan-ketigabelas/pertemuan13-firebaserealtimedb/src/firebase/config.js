121ebfc3d2a1fa5327df28d7841da7a18e9459d1
Angga Maulana Athaariq <am.athaariq@gmail.com>
2020-04-30 10:11:43 +0700

    firebase realtime database

---
+++ pertemuan-ketigabelas/pertemuan13-firebaserealtimedb/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdQ3tYuW5ROf4RJEKb6mTItFamH68mtcw",
+    authDomain: "fir-login-fb961.firebaseapp.com",
+    databaseURL: "https://fir-login-fb961.firebaseio.com",
+    projectId: "fir-login-fb961",
+    storageBucket: "fir-login-fb961.appspot.com",
+    messagingSenderId: "575074659606",
+    appId: "1:575074659606:web:775ea4ef477c9c3677b437",
+    measurementId: "G-5WCF02BYX0"
+  };
+
+  export default firebaseConfig;
